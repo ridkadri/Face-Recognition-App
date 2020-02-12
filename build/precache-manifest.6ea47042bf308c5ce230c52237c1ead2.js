419c642ec6dd78653f77d4c93f6fc5c20d237b46
Ridwan <kadriridwan0@gmail.com>
2020-02-12 13:03:03 +0000

    Cleaning up dependancies

---
+++ build/precache-manifest.6ea47042bf308c5ce230c52237c1ead2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b285cbf2658bce7cfa30ecc89bc1514",
+    "url": "/Face-Recognition-App/index.html"
+  },
+  {
+    "revision": "e257dc336ecc8d4bd4be",
+    "url": "/Face-Recognition-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "942843e4325445cd0163",
+    "url": "/Face-Recognition-App/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "e257dc336ecc8d4bd4be",
+    "url": "/Face-Recognition-App/static/js/2.7e259ea4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Face-Recognition-App/static/js/2.7e259ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942843e4325445cd0163",
+    "url": "/Face-Recognition-App/static/js/main.af70624a.chunk.js"
+  },
+  {
+    "revision": "d7f4617fbc1e2cbd69b1",
+    "url": "/Face-Recognition-App/static/js/runtime-main.a56e2f86.js"
+  }
+]);
